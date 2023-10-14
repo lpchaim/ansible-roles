dcceb77a1ba9dc5dda05cb38e5acfa5e670649da
Lucas Chaim <lpchaim@gmail.com>
2023-10-14 13:41:28 -0300

    fix: Nushell config files
    Added blank strings to the main file contents variables. It seems Home Manager would try to use the default contents when omitted, but my custom config already covers that.

--- home-manager/templates/nushell/module.nix
+++ home-manager/templates/nushell/module.nix
@@ -2,6 +2,8 @@
 {
   programs.nushell = {
     enable = true;
+    configFile.text = "";
+    envFile.text = "";
     extraConfig = (builtins.readFile ./config.nu);
     extraEnv = (builtins.readFile ./env.nu);
   };
