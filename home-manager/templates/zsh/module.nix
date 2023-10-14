cb431bcccd686744e1ae28e7c3534d55b456273d
Lucas Chaim <lpchaim@gmail.com>
2023-10-14 13:43:57 -0300

    feat: ZSH config tweaks
    Added last-working-dir plugin
    
    Added zsh-nix-shell plugin
    
    Removed manually sourced Nix/HM files, as that's not actually needed at this point
    
    Set COLORTERM environment variable to truecolor so WSL picks it up

--- home-manager/templates/zsh/module.nix
+++ home-manager/templates/zsh/module.nix
@@ -9,16 +9,28 @@
       ignoreDups = true;
     };
     initExtra = ''
-      if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then source "$HOME/.nix-profile/etc/profile.d/nix.sh"; fi
-      if [ -e "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"; fi
+      export COLORTERM=truecolor
     '';
     oh-my-zsh = {
       enable = true;
       plugins = [
         "git"
         "gitfast"
+        "last-working-dir"
       ];
       theme = "agnoster";
     };
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.7.0";
+          sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
+        };
+      }
+    ];
   };
 }
